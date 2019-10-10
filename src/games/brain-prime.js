da9421cbb339a975c26eb5400a1fa417592c6c8f
Victoria Antonova <gershovavictoria@gmail.com>
2019-10-10 12:49:47 +0200

    Change isPrimeNumber function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,8 +5,8 @@ const ansYes = 'yes';
 const ansNo = 'no';
 const gameDescription = `Answer ${ansYes} if given number is prime. Otherwise answer ${ansNo}`;
 
-const isPrimeNumber = (num) => {
-  for (let factor = 2; factor < num; factor += 1) {
+const isPrime = (num) => {
+  for (let factor = 2; factor <= num / 2; factor += 1) {
     if (num % factor === 0) {
       return false;
     }
@@ -16,8 +16,7 @@ const isPrimeNumber = (num) => {
 
 const getNumIsPrime = () => {
   const num = Math.floor(Math.random() * 100) + 1;
-  const isPrime = isPrimeNumber(num);
-  return cons(num, isPrime ? ansYes : ansNo);
+  return cons(num, isPrime(num) ? ansYes : ansNo);
 };
 
 export default () => {
