a61ff724be123392b190cfd248c00c10bf690c8b
Victoria Antonova <gershovavictoria@gmail.com>
2019-10-15 11:18:01 +0200

    Add var length for func randomArithmeticProgression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,18 +3,18 @@ import runBrainGame from '..';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const randomArithmeticProgression = () => {
-  const dif = Math.floor(Math.random() * 11);
+const randomArithmeticProgression = (length) => {
+  const diff = Math.floor(Math.random() * 11);
   const firstNum = Math.floor(Math.random() * 11);
   const progression = [];
-  for (let i = 1; i <= 10; i += 1) {
-    progression.push(firstNum + dif * i);
+  for (let i = 0; i < length; i += 1) {
+    progression.push(firstNum + diff * i);
   }
   return progression;
 };
 
 const getPrograssionQuestion = () => {
-  const progression = randomArithmeticProgression();
+  const progression = randomArithmeticProgression(10);
   const randTermInd = Math.floor(Math.random() * 10);
   let question = '';
   for (let i = 0; i < progression.length; i += 1) {
