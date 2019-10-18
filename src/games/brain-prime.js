48d09752503d7d02764ae30a0515e5fd120e3ff2
Victoria Antonova <gershovavictoria@gmail.com>
2019-10-18 06:30:56 +0200

    Add a condition for 0, 1 and negative numbers to isPrime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,6 +5,9 @@ import runBrainGame from '..';
 const gameDescription = "Answer 'yes' if given number is prime. Otherwise answer 'no'";
 
 const isPrime = (num) => {
+  if (num <= 0 || num === 1) {
+    return false;
+  }
   for (let factor = 2; factor <= num / 2; factor += 1) {
     if (num % factor === 0) {
       return false;
