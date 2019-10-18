f4616439433c56689266127574400b11aa2a0c8a
Victoria Antonova <gershovavictoria@gmail.com>
2019-10-18 07:05:19 +0200

    Make randomArithmeticProgression function pure

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,9 +5,7 @@ import runBrainGame from '..';
 const gameDescription = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const randomArithmeticProgression = (length) => {
-  const diff = getRandomNum(1, 10);
-  const firstTerm = getRandomNum(0, 10);
+const randomArithmeticProgression = (firstTerm, diff, length) => {
   const progression = [];
   for (let i = 0; i < length; i += 1) {
     progression.push(firstTerm + diff * i);
@@ -16,17 +14,19 @@ const randomArithmeticProgression = (length) => {
 };
 
 const makeQuestionWithAnswer = () => {
-  const progression = randomArithmeticProgression(progressionLength);
-  const randTermInd = getRandomNum(0, progressionLength - 1);
+  const diff = getRandomNum(1, 10);
+  const firstTerm = getRandomNum(0, 10);
+  const progression = randomArithmeticProgression(firstTerm, diff, progressionLength);
+  const skipTermInd = getRandomNum(0, progressionLength - 1);
   let question = '';
   for (let i = 0; i < progressionLength; i += 1) {
-    if (i === randTermInd) {
+    if (i === skipTermInd) {
       question = `${question} ..`;
     } else {
       question = `${question} ${progression[i]}`;
     }
   }
-  const answer = progression[randTermInd];
+  const answer = progression[skipTermInd];
   return cons(question, answer);
 };
 
